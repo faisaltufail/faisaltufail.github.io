64c7edc8e5d9c68e3fb51a4f9bb3e6392c3de35e
faisaltufail <rana.faisal998@gmail.com>
2021-05-17 03:01:00 +0500

    Updates

---
+++ precache-manifest.4d0353942b79f0570ab5ceb893f44447.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf284425be104b471a00d5efe8caa45",
+    "url": "/faisaltufail.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "6cd4e2506451e6817df7",
+    "url": "/faisaltufail.github.io/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "1df20badaa6c6711e7f2",
+    "url": "/faisaltufail.github.io/portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "d599091335a18a356cd0",
+    "url": "/faisaltufail.github.io/portfolio/static/css/main.bd80e8bd.chunk.css"
+  },
+  {
+    "revision": "397222d318c31b92edf1",
+    "url": "/faisaltufail.github.io/portfolio/static/js/2.812be988.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/faisaltufail.github.io/portfolio/static/js/2.812be988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd4e2506451e6817df7",
+    "url": "/faisaltufail.github.io/portfolio/static/js/3.dd7efb97.chunk.js"
+  },
+  {
+    "revision": "1df20badaa6c6711e7f2",
+    "url": "/faisaltufail.github.io/portfolio/static/js/4.5cc84415.chunk.js"
+  },
+  {
+    "revision": "d599091335a18a356cd0",
+    "url": "/faisaltufail.github.io/portfolio/static/js/main.5c10e040.chunk.js"
+  },
+  {
+    "revision": "a9db7ad83dec0576cf23",
+    "url": "/faisaltufail.github.io/portfolio/static/js/runtime-main.96a7edc5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/faisaltufail.github.io/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/faisaltufail.github.io/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3c1761709f0c0058f4eaf9b4a13528c7",
+    "url": "/faisaltufail.github.io/portfolio/static/media/code.3c176170.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/faisaltufail.github.io/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/faisaltufail.github.io/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ca0924fc7f0a313547e6c570f7032dda",
+    "url": "/faisaltufail.github.io/portfolio/static/media/digiskill.ca0924fc.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/faisaltufail.github.io/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/faisaltufail.github.io/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/faisaltufail.github.io/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/faisaltufail.github.io/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/faisaltufail.github.io/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
